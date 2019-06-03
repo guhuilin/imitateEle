00f7bb1490ecbf8ec93bfa9320d138aeb63214dd
zilan <1835262016@qq.com>
2019-06-03 21:50:40 +0800

    prolist

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,12 @@ Vue.use(Vuex);
 
 // 引入模块
 import index from './modules/index'
+import proList from './modules/productList'
 
 export default new Vuex.Store({
   modules: {
-    index
+    index,
+    proList
   },
   plugins: [Logger()]
 })
