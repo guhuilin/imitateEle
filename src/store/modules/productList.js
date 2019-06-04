9c46aa2f384780510dffb73bb59cd6d85271de08
zilan <1835262016@qq.com>
2019-06-04 12:05:28 +0800

    filter

--- src/store/modules/productList.js
+++ src/store/modules/productList.js
@@ -1,7 +1,46 @@
 import {getProductList} from "@/api/proList"
 
 const state = {
-  listData:[]
+  listData:[],
+  filterbusData:[
+    {
+      title:'品',
+      checkCont:'品牌商家',
+      color:'rgb(63, 189, 230)',
+      checked:false
+    },
+    {
+      title:'保',
+      checkCont:'外卖保',
+      color: 'rgb(153, 153, 153)',
+      checked:false
+    },
+    {
+      title:'准',
+      checkCont:'准时达',
+      color:'rgb(63, 189, 230)',
+      checked:false
+      
+    },
+    {
+      title:'新',
+      checkCont:'新店',
+      color: 'rgb(255, 78, 0)',
+      checked:false
+    },
+    {
+      title:'付',
+      checkCont:'在线支付',
+      color: 'rgb(255, 78, 0)',
+      checked:false
+    },
+    {
+      title:'票',
+      checkCont:'开发票',
+      color: 'rgb(153, 153, 153)',
+      checked:false
+    }
+  ]
 }
 
 const mutations = {
